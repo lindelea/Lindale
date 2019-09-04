0e8a351789a025eb599fad0e1d2ef97bfe188548
lindelea <kgo@lindelin.org>
2019-09-04 18:30:47 +0900

    ログイン画面

--- app/src/main/java/org/lindelin/lindale/activities/LoginActivity.kt
+++ app/src/main/java/org/lindelin/lindale/activities/LoginActivity.kt
@@ -2,12 +2,26 @@ package org.lindelin.lindale.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
 import org.lindelin.lindale.R
+import org.lindelin.lindale.models.OAuth
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun loginButtonHasTapped(view: View) {
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
+
+        OAuth.login(email, password) {
+            it?.let {
+                println(it.accessToken)
+            }
+        }
     }
 }
