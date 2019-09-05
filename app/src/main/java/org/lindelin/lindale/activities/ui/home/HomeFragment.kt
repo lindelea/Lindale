40a93bd72c3374cf179aff7e1666811dc9646fff
lindelea <kgo@lindelin.org>
2019-09-05 18:30:13 +0900

    HOME 画面実装

--- app/src/main/java/org/lindelin/lindale/activities/ui/home/HomeFragment.kt
+++ app/src/main/java/org/lindelin/lindale/activities/ui/home/HomeFragment.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.fragment_home.view.*
 import org.lindelin.lindale.R
+import org.lindelin.lindale.models.Profile
+import org.lindelin.lindale.supports.setImageFromUrl
 
 class HomeFragment : Fragment() {
+
+    private var profile: Profile? = null
 
     private lateinit var homeViewModel: HomeViewModel
 
@@ -22,10 +25,25 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
+        loadData {
+            root.profileImageView.setImageFromUrl(profile?.photo)
+            root.projectCountText.text = profile?.status?.projectCount.toString()
+            root.taskCountText.text = profile?.status?.unfinishedTaskCount.toString()
+            root.todoCountText.text = profile?.status?.unfinishedTodoCount.toString()
+        }
+
         return root
+    }
+
+    fun loadData(callback: () -> Unit = {}) {
+        activity?.let {
+            Profile.feach(it) { profile ->
+                profile?.let {
+                    this.profile = it
+                    callback()
+                }
+            }
+        }
     }
 }
