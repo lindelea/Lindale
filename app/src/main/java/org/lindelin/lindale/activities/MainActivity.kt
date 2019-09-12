384706a459a3cf0b1f6b124943adac119b63182b
lindelea <kgo@lindelin.org>
2019-09-12 16:09:10 +0900

    created settings menu

--- app/src/main/java/org/lindelin/lindale/activities/MainActivity.kt
+++ app/src/main/java/org/lindelin/lindale/activities/MainActivity.kt
@@ -64,11 +64,11 @@ class MainActivity : AppCompatActivity(), FavoriteFragment.OnListFragmentInterac
         })
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        menuInflater.inflate(R.menu.main, menu)
+//        return true
+//    }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
